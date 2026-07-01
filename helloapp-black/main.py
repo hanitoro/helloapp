b160e3d8eca1115ef98bd7be14d7adc2a717d5bb
black <black@example.com>
2026-07-01 23:37:35 +0500

    Add greetBlack function

--- helloapp-black/main.py
+++ helloapp-black/main.py
@@ -1,5 +1,11 @@
 def say_hello():
     print("hello everyone")
 
+def greetWhite():
+    print("hello white")
+
+def greetBlack():
+    print("hello black")
+
 if __name__ == "__main__":
     say_hello()
